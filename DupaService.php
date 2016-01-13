2cfb1ff130ab86830b0c8eb6083b43d9b49d607e
pgdba <jaymzxii@gmail.com>
2016-01-13 20:41:32 +0100

    test service

---
+++ DupaService.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pgdba
+ * Date: 13.01.16
+ * Time: 20:39
+ */
+
+namespace Pg\TestBundle;
+
+
+class DupaService
+{
+
+    public function doDupa()
+    {
+        var_dump($_SERVER);
+        exit;
+    }
+}
